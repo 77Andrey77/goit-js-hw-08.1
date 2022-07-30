26f86f53fb7a3e3543fe71eadd699efeb73f68be
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2022-07-30 13:50:14 +0300

    add all

---
+++ js/renderGallery.js
@@ -0,0 +1,94 @@
+import galleryItems from '../gallery-items.js';
+
+const galleruEL = document.querySelector('.js-gallery');
+const lightboxEl = document.querySelector('.js-lightbox');
+const lightboxImageEl = document.querySelector('.lightbox__image');
+const lightboxButtonEl = document.querySelector('.lightbox__button');
+// const lightboxOverlayEl = document.querySelector('.lightbox__overlay');
+
+// одна карточка
+const createItem = (option) => {
+  const liEl = document.createElement('li');
+  liEl.classList.add('gallery__item');
+
+  const linkEl = document.createElement('a');
+  liEl.appendChild(linkEl);
+  linkEl.classList.add('gallery__link');
+  // linkEl.href = option.original;
+
+  const imgEL = document.createElement('img');
+  linkEl.appendChild(imgEL);
+  imgEL.classList.add('gallery__image');
+  imgEL.loading = 'lazy';
+  imgEL.src = option.preview;
+  imgEL.alt = option.description;
+  imgEL.dataset.source = option.original;
+
+  return liEl;
+};
+// масив карточек
+const createGalerry = galleryItems.map(createItem);
+
+// распыляем карточки в разметку
+galleruEL.append(...createGalerry);
+
+//////////////////////////////////////////////////////////////////////////
+
+galleruEL.addEventListener('click', onOpenModal);
+
+// open modal
+function onOpenModal(evt) {
+  //  even.preventDefault(); 
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  };
+  // if (even.target.nodeName !== 'IMG') {
+  //       return;
+  // };
+  
+
+  lightboxEl.classList.add('is-open');
+  
+  lightboxImageEl.src = evt.target.dataset.source;
+  lightboxImageEl.alt = evt.target.alt;
+
+lightboxButtonEl.addEventListener('click', onCloseModal);
+lightboxEl.addEventListener('click', onCloseModalOverlay);
+window.addEventListener('keydown', onCloseModalESC);
+};
+
+
+
+//close modal///////////////////////////////////////////////////
+
+function onCloseModal() {
+  lightboxEl.classList.remove('is-open');
+  lightboxImageEl.src = '';
+  lightboxImageEl.alt = '';
+
+lightboxButtonEl.removeEventListener('click', onCloseModal);
+lightboxEl.removeEventListener('click', onCloseModalOverlay);
+window.removeEventListener('keydown', onCloseModalESC);
+};
+
+//close modal by overlay////////////////////////////////////////
+
+function onCloseModalOverlay(evt) {
+  if (!evt.target.classList.contains('lightbox__overlay')) {
+    return;
+  };
+
+  onCloseModal();
+};
+
+
+//close modal by Escape//////////////////////////////////////////
+
+function onCloseModalESC(evt) {
+  // console.log(evt.key);
+  if (evt.key !== 'Escape') {
+    return;
+  }
+  onCloseModal();
+
+}
